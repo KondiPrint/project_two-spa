254a1ba329005b9aa91fb862a4e94e8ba9130d7c
KondiPrint <Marvin.kn@protonmail.com>
2023-11-21 14:42:13 +0100

    v.0.9.8

--- src/app/getData.ts
+++ src/app/getData.ts
@@ -1,5 +1,7 @@
 export async function getData() {
-  const data = await fetch("http://localhost:3000/assets/content_data.json");
+  const data = await fetch(
+    "https://mocki.io/v1/8a4d30ef-78b5-4cb2-8ad6-fe0d7dd08e0e"
+  );
 
   if (!data.ok) {
     throw new Error("failed");
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -9,33 +9,39 @@ interface Props {
 }
 
 export default function Modal({ children, src, alt, email }: Props) {
+  // * Laver et state for mit modal.
   const [modalDisplay, setModalDisplay] = useState("none");
-  const formRef = useRef<HTMLFormElement | null>(null);
 
+  // * Her laver jeg en const som jeg kan bruge som et reset til min form.
+  const resetForm = useRef<HTMLFormElement | null>(null);
+
+  // * Her resetter jeg al text / data inde i min modal form, så det altid er en tom form som åbnes.
+  const reset = () => {
+    if (resetForm.current) {
+      resetForm.current.reset();
+    }
+  };
+
+  // * Her laver jeg en funktion til at vise min modal.
   const openModal = () => {
     setModalDisplay("block");
   };
 
+  // * Her laver jeg en funktion for at lukke min modal.
   const closeModal = () => {
     setModalDisplay("none");
-    reset()
+    reset();
   };
 
-  const reset = () => {
-    if (formRef.current) {
-      formRef.current.reset();
-    }
-  };
-
+  // * Her laver jeg en onClick event, så når jeg clicker udenfor min form, når modalen er åben, lukker den min modal.
   window.onclick = event => {
     const eventTarget = event.target as Element | null;
 
     if (eventTarget?.matches("#modal")) {
       closeModal();
-      reset()
+      reset();
     }
   };
-
 
   return (
     <>
@@ -47,11 +53,13 @@ export default function Modal({ children, src, alt, email }: Props) {
           <p>{email}</p>
         </figcaption>
       </figure>
-
+      {/* Her sørger jeg for min modal bruger min useState til "display"
+      optionen. */}
       <div id="modal" style={{ display: modalDisplay }} className={style.modal}>
         <form
           className={`${style.modal_content} ${style.animate}`}
-          method="post" name="myForm" ref={formRef}
+          name="myForm"
+          ref={resetForm}
         >
           <div className={style.container}>
             <h2>{children}</h2>
@@ -67,14 +75,16 @@ export default function Modal({ children, src, alt, email }: Props) {
             </div>
 
             <div>
+              <label htmlFor="text">Subject</label>
+              <input type="text" name="text" required />
+            </div>
+            <div>
               <label htmlFor="comment">Comment</label>
-              <textarea name="comment"></textarea>
+              <textarea name="comment" required></textarea>
             </div>
 
             <div className={style.modal__btn}>
-              <button type="button" onClick={closeModal}>
-                Submit
-              </button>
+              <button type="submit">Submit</button>
               <button type="button" onClick={closeModal}>
                 Close
               </button>
